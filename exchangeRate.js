e6046e46448bfcd08fd8523a351e09974e23b13b
nacho-cs <119465751+nacho-cs@users.noreply.github.com>
2022-12-11 15:41:04 -0500

    Update exchangeRate.js
    prevented negative values

--- exchangeRate.js
+++ exchangeRate.js
@@ -2,6 +2,10 @@ const USD = document.getElementById("amount");
 const output = document.getElementById("output");
 
 const exchangeRate = async () => {
+  if (USD.value < 0.01) {
+    output.innerHTML = "Please enter a number greeater than 0";
+    return;
+  }
   const response = await fetch("https://open.er-api.com/v6/latest/USD");
   const data = await response.json();
   const rounded = Math.round(100 * (USD.value * data.rates.CAD)) / 100
